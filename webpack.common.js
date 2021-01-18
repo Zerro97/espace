68e18a9d8328512de070012801e2852b986e68c5
Guest <guest-xf5kzt@h26118.sgb.skir.kr>
2021-01-18 20:14:46 +0900

    Initial Project Set Up

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      }
+    ]
+  }
+}
