5fe7c53bf0f8ef3a7568b5d2ee936ab7a23bd814
Zerro97 <khuibeom@gmail.com>
2021-02-05 23:38:05 +0900

    Worked on dashboard: equipment page and map page

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,20 +1,38 @@
 import './dashboard.scss';
 import '../assets/logo.png';
 
-let statusPage = document.getElementById("status");
-let equipmentPage = document.getElementById("equipment");
-let mapPage = document.getElementById("map");
-
+let currentPage = "";
 let pages = document.getElementsByClassName("dashboard__main");
 let links = document.getElementsByClassName("dashboard__menuText");
 
-links.forEach((link) => {
+for (let link of links) {
     link.addEventListener("click", () => {
-        statusPage.style.display = "none";
-        equipmentPage.style.display = "flex";
-        mapPage.style.display = "none";
+        for (let link of links) {
+            link.className = link.className.replace(/\bactiveLink\b/g, "");
+        }
+
+        for (let page of pages) {
+            page.className = page.className.replace(/\bshow\b/g, "");
+        }
+
+        link.classList.add("activeLink");
+        currentPage = link.getAttribute("name");
+        document.getElementById(currentPage).classList.add("show");
+
+        if (currentPage === "equipmentPage") {
+            let boxes = "";
+            for (let i = 0; i < 56; i++) {
+                boxes += `<div class="equip__box"></div>`;
+            }
+
+            let equipments = document.getElementById("equip__container");
+            console.log(equipments);
+            equipments.innerHTML = boxes;
+        }
     });
-})
+}
+
+
 
 
 // statusLink.addEventListener("click", () => {
